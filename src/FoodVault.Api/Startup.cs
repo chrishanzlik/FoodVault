347fdbf859afc44e1819b4c4e3cd41f4073d7fb7
Christopher Hanzlik <senz2702@gmail.com>
2020-12-22 14:03:04 +0100

    Remove default controller

--- src/FoodVault.Api/Startup.cs
+++ src/FoodVault.Api/Startup.cs
@@ -23,7 +23,6 @@ namespace FoodVault.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -40,7 +39,6 @@ namespace FoodVault.Api
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
