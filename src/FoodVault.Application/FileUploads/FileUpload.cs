0c3ce372503584c3456a22722a044d606eb0664d
Christopher Hanzlik <senz2702@gmail.com>
2021-01-23 23:06:54 +0100

    SQL statements for file upload repo

--- src/FoodVault.Application/FileUploads/FileUpload.cs
+++ src/FoodVault.Application/FileUploads/FileUpload.cs
@@ -1,10 +1,10 @@
-﻿using FoodVault.Domain;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
 
 namespace FoodVault.Application.FileUploads
 {
+    /// <summary>
+    /// Represents meta information to an file upload.
+    /// </summary>
     public class FileUpload
     {
         /// <summary>
@@ -15,6 +15,15 @@ namespace FoodVault.Application.FileUploads
 
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileUpload" /> class.
+        /// </summary>
+        /// <param name="id">Files identifier.</param>
+        /// <param name="relativePath">Files relative paath from root folder.</param>
+        /// <param name="extension">File extension.</param>
+        /// <param name="contentType">Files content-type.</param>
+        /// <param name="size">Size of the uploaded file.</param>
+        /// <param name="expirationTime">When the files expires.</param>
         public FileUpload(Guid id, string relativePath, string extension, string contentType, long size, DateTime? expirationTime)
         {
             Id = id;
@@ -26,32 +35,58 @@ namespace FoodVault.Application.FileUploads
             ExpirationTime = expirationTime;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileUpload" /> class.
+        /// </summary>
+        /// <param name="relativePath">Files relative paath from root folder.</param>
+        /// <param name="extension">File extension.</param>
+        /// <param name="contentType">Files content-type.</param>
+        /// <param name="size">Size of the uploaded file.</param>
+        /// <param name="expirationTime">When the files expires.</param>
         public FileUpload(string relativePath, string extension, string contentType, long size, DateTime? expirationTime)
+            : this(Guid.NewGuid(), relativePath, extension, contentType, size, expirationTime)
         {
-            Id = Guid.NewGuid();
-            UploadTime = DateTime.UtcNow;
-            RelativeFileLocation = relativePath;
-            Size = size;
-            ContentType = contentType;
-            Extension = extension;
-            ExpirationTime = expirationTime;
+
         }
 
+        /// <summary>
+        /// Gets the identifier.
+        /// </summary>
         public Guid Id { get; }
+
+        /// <summary>
+        /// Gets the files content-type.
+        /// </summary>
         public string ContentType { get; }
 
         /// <summary>
-        /// w/o leading dot
+        /// Gets the file extension without leading dot.
         /// </summary>
         public string Extension { get; }
-        public string RelativeFileLocation { get; }
-        public long Size { get; }
-        public DateTime? ExpirationTime { get; private set; }
+
         /// <summary>
-        /// utc
+        /// Gets the files relative stored loaction. (Relative from FileUploadSettings.RootPath)
+        /// </summary>
+        public string RelativeFileLocation { get; }
+
+        /// <summary>
+        /// Gets the file size.
+        /// </summary>
+        public long Size { get; }
+
+        /// <summary>
+        /// Gets the expiration date of the file. If this property is null, the file is peristed.
+        /// </summary>
+        public DateTime? ExpirationTime { get; private set; }
+        
+        /// <summary>
+        /// Gets the upload time.
         /// </summary>
         public DateTime UploadTime { get; }
 
+        /// <summary>
+        /// Persists the file to the storage.
+        /// </summary>
         public void Persist()
         {
             ExpirationTime = null;
--- src/FoodVault.Infrastructure/FileUploads/FileUploadSqlRepository.cs
+++ src/FoodVault.Infrastructure/FileUploads/FileUploadSqlRepository.cs
@@ -1,8 +1,8 @@
-﻿using FoodVault.Application.Database;
+﻿using Dapper;
+using FoodVault.Application.Database;
 using FoodVault.Application.FileUploads;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FoodVault.Infrastructure.FileUploads
@@ -14,6 +14,10 @@ namespace FoodVault.Infrastructure.FileUploads
     {
         private readonly IDbConnectionFactory _dbConnectionFactory;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileUploadSqlRepository" /> class.
+        /// </summary>
+        /// <param name="dbConnectionFactory">SQL db connection factory.</param>
         public FileUploadSqlRepository(IDbConnectionFactory dbConnectionFactory)
         {
             _dbConnectionFactory = dbConnectionFactory;
@@ -22,41 +26,54 @@ namespace FoodVault.Infrastructure.FileUploads
         /// <inheritdoc />
         public Task AddAsync(FileUpload fileUpload)
         {
+            const string insertSql =
+                "INSERT INTO [dbo].[FileUploads](Id, ContentType, Extension, RelativeFileLocation, Size, ExpirationTime, UploadTime) " +
+                "VALUES (@Id, @ContentType, @Extension, @RelativeFileLocation, @Size, @ExpirationTime, @UploadTime)";
+
             var connection = _dbConnectionFactory.GetOpen();
 
-            throw new NotImplementedException();
+            return connection.ExecuteAsync(insertSql, fileUpload);
         }
 
         /// <inheritdoc />
         public Task<FileUpload> GetByIdAsync(Guid id)
         {
+            const string querySql = "SELECT * FROM [dbo].[FileUploads] WHERE [Id] = @id";
+
             var connection = _dbConnectionFactory.GetOpen();
 
-            throw new NotImplementedException();
+            return connection.QueryFirstOrDefaultAsync<FileUpload>(querySql, new { id });
         }
 
         /// <inheritdoc />
         public Task<IEnumerable<FileUpload>> GetExpiredFilesAsync()
         {
+            const string querySql =
+                "SELECT * FROM [dbo].[FileUploads] WHERE [ExpirationTime] IS NOT NULL AND [ExpirationTime] < @now";
+
             var connection = _dbConnectionFactory.GetOpen();
 
-            throw new NotImplementedException();
+            return connection.QueryAsync<FileUpload>(querySql, new { now = DateTime.UtcNow });
         }
 
         /// <inheritdoc />
         public Task PersistFileAsync(Guid id)
         {
+            const string persistSql = "UPDATE [dbo].[FileUploads] SET [ExpirationTime] = NULL WHERE [Id] = @id";
+
             var connection = _dbConnectionFactory.GetOpen();
 
-            throw new NotImplementedException();
+            return connection.ExecuteAsync(persistSql, new { id });
         }
 
         /// <inheritdoc />
         public Task RemoveAsync(Guid id)
         {
+            const string removeSql ="DELETE FROM [dbo].[FileUploads] WHERE [Id] = @id";
+
             var connection = _dbConnectionFactory.GetOpen();
 
-            throw new NotImplementedException();
+            return connection.ExecuteAsync(removeSql, new { id });
         }
     }
 }
